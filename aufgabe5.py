695d90a00ef19a91a9c16e2cc856789d439b9af7
Adrian <adrian.abbassian@haw-hamburg.de>
2016-06-03 14:57:31 +0200

    newAufgabe5
    Vergleichen den Matrizen

--- aufgabe5.py
+++ aufgabe5.py
@@ -12,6 +12,28 @@ import numpy as np
 #   4. Ist das Ergebnis nicht identisch vorhandende Einsen in der Zeile um eine Stelle nach rechts verschieben
 #   5. Indizies mit 2er-Potenzen summieren und Ergbnis ausgeben 
 
+
+def matrixRowCol(level,rowCol,rowColZeros,length):
+    for i in range(level):
+        #gucken wieviele Zahlen in row/col sind
+        if len(length[i]) > 1:
+            #wert1 enthält den ersten Wert aus row und col und Wert zwei den zweiten
+            wert1 = rowCol[i][0]
+            wert2 = rowCol[i][1]
+            #filtern der [1,2] oder [2,1] oder [1,1] Kombination
+            if wert1 != wert2:
+                #Startwert für die Zweite Zahl aus row und col
+                wert2 = level-wert2
+                rowColZeros[i][:wert1:1] = 1
+                rowColZeros[i][wert2:level:1] = 1
+            else:
+                #Fall für [1,1]
+                rowColZeros[i][0:level:2] = 1
+        #Fall für einzelne Zahlen wie [1],[2].......      
+        else:
+            wert = rowCol[i][0]
+            rowColZeros[i][:wert:1] = 1
+    return rowColZeros
     
 # Decodierung für maximal 4 x 4 Matritzen 
 def decode(rowSpec, colSpec):
@@ -26,32 +48,40 @@ def decode(rowSpec, colSpec):
     rows = np.array([rowSpec]).flatten()
     cols = np.array([colSpec]).flatten()
     #print(rows)
-   # print(cols)
-
+    #print(cols)
+    
 #rows<--------------------------------
+    rowMatrix = matrixRowCol(level,rows,rows_zeros,rowSpec)
+    print(rowMatrix)
+#Cols<----------------------------------
+    colMatrix = matrixRowCol(level,cols,cols_zeros,colSpec).T
+    print(colMatrix)
+    
     for i in range(level):
-        if len(rowSpec[i]) > 1:
-            wert1 = rows[i][0]
-            wert2 = rows[i][1]
-            if wert1 != wert2:
-                wert2 = level-wert2
-                rows_zeros[i][:wert1:1] = 1
-                rows_zeros[i][wert2:level:1] = 1
-            else:
-                rows_zeros[i][0:level:2] = 1
-                
-        else:
-            wert = rows[i][0]
-            rows_zeros[i][:wert:1] = 1
-            
-   
-      
-    print(rows_zeros)
- 
+        valueRow = 0
+        valueCol = 0
+        for j in range(level):
+            valueRow = valueRow + rowMatrix[j][i]
+            valueCol = valueCol + colMatrix[j][i]
+        print(valueRow)
+        print(valueCol)
+        if valueRow == valueCol:
+            print("nice")
+        if valueRow != valueCol:
+            print("not Nice")
+            valueRow
+    
+
+    
+  
+    
+    
+    
+
+
     # 2 x 2 Matritzen abarbeiten
    
     # Test-Ausgabe der befüllten Arrays            
-    #print(rows_zeros)
-    #print(cols_zeros)       
+  
 # 2 x 2 Test (Leerzeichen erstmal mit 0 ersetzt)
 decode([[1], [1, 1], [2, 1], [1, 2]], [[3], [1, 1], [1, 1], [2]])
